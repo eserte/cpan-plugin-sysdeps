88b3509a32bcfe7d7b03c9fca36a81efa8cc134a
Slaven Rezic <slaven@rezic.de>
2016-09-17 21:07:19 +0200

    0.16

--- lib/CPAN/Plugin/Sysdeps.pm
+++ lib/CPAN/Plugin/Sysdeps.pm
@@ -3,7 +3,7 @@ package CPAN::Plugin::Sysdeps;
 use strict;
 use warnings;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use Hash::Util 'lock_keys';
 use List::Util 'first';
--- lib/CPAN/Plugin/Sysdeps/Mapping.pm
+++ lib/CPAN/Plugin/Sysdeps/Mapping.pm
@@ -3,7 +3,7 @@ package CPAN::Plugin::Sysdeps::Mapping;
 use strict;
 use warnings;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 # shortcuts
 #  os and distros
