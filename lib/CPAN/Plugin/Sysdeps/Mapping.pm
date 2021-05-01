e8969351cec683ad5e2c8ab9dc5ca0c3dbf070e5
Slaven Rezic <slaven@rezic.de>
2021-05-01 11:16:15 +0200

    mapping update for JavaScript::V8

--- lib/CPAN/Plugin/Sysdeps/Mapping.pm
+++ lib/CPAN/Plugin/Sysdeps/Mapping.pm
@@ -2022,10 +2022,10 @@ sub mapping {
        [package => 'v8']],
       [like_debian,
        [before_debian_buster,
-	[package => 'libv8-dev']], # but not anymore in buster, see https://tracker.debian.org/news/876959/libv8-314-removed-from-testing/
-       [package => 'libnode-dev']], # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934734 (but perl module does not pick v8.h from the installed location)
+	[package => [qw(libv8-3.14.5 libv8-3.14-dev)]]], # but still does not work, libplatform/libplatform.h missing
+       [package => 'libnode-dev']], # # libv8* not anymore in buster, see https://tracker.debian.org/news/876959/libv8-314-removed-from-testing/; https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934734 (but perl module does not pick v8.h from the installed location)
       [like_fedora,
-       [package => 'v8-devel']], # but problems with Devel-CheckLib and compilation errors
+       [package => 'v8-devel']], # but problems with Devel-CheckLib (centos7) and compilation errors (fedora), both with JavaScript-V8-0.10
       [os_darwin,
        [package => 'v8']], # but compilation errors (v8-5.0.71.33 <-> JavaScript-V8-0.07)
      ],
