76639da80599c7b5a7ba4e3775d699e3fdc4fe73
Slaven Rezic <slaven@rezic.de>
2026-04-23 11:21:30 +0200

    0.81

--- lib/CPAN/Plugin/Sysdeps.pm
+++ lib/CPAN/Plugin/Sysdeps.pm
@@ -3,7 +3,7 @@ package CPAN::Plugin::Sysdeps;
 use strict;
 use warnings;
 
-our $VERSION = '0.80';
+our $VERSION = '0.81';
 
 use List::Util 'first';
 
@@ -1134,7 +1134,7 @@ Slaven Rezic
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2016,2017,2018,2019,2024 by Slaven ReziE<x0107>
+Copyright (C) 2016,2017,2018,2019,2024,2025,2026 by Slaven ReziE<x0107>
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
--- lib/CPAN/Plugin/Sysdeps/Mapping.pm
+++ lib/CPAN/Plugin/Sysdeps/Mapping.pm
@@ -3,7 +3,7 @@ package CPAN::Plugin::Sysdeps::Mapping;
 use strict;
 use warnings;
 
-our $VERSION = '0.80';
+our $VERSION = '0.81';
 
 # shortcuts
 #  os and distros
